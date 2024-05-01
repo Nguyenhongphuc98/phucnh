5fd80db48baa78a088c710bb8d73c45fa7936345
phucnh7 <phucnh7@vng.com.vn>
2024-05-01 22:31:56 +0700

    chore

--- app/tech/[id]/page.tsx
+++ app/tech/[id]/page.tsx
@@ -7,6 +7,7 @@ export default function TechPost() {
   const path = usePathname();
   const dirs = path.split('/');
 
-  const data = TechPosts[dirs[dirs.length - 1]];
-  return <>{data.refs.map((r) => r)}</>;
+  // const data = TechPosts[dirs[dirs.length - 1]];
+  // return <>{data.refs.map((r) => r)}</>;
+  return <h1>{dirs[dirs.length - 1]}</h1>
 }
